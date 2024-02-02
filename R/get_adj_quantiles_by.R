19310906d3b94ea211f1ebf4cbd0bcd252bf57a3
Pierre-Yves de Müllenheim <pierre-yves.demullenheim@hotmail.fr>
2024-02-02 21:52:48 +0100

    clean code

--- R/get_adj_quantiles_by.R
+++ R/get_adj_quantiles_by.R
@@ -1,17 +1,22 @@
 # Get adjusted quantiles
 get_adj_quantiles_by <- function(x, by, design) {
+  
+  # Compute quantiles
   df <-
-    svyby(as.formula(paste0( "~" , x)), 
-          by = as.formula(paste0( "~" , by)), 
-          design = design,
-          FUN = svyquantile,
-          quantile = c(0, 0.25, 0.5, 0.75, 1),
-          vartype = "ci",
-          level = 0.95
+    survey::svyby(
+      formula = as.formula(paste0( "~" , x)),
+      by = as.formula(paste0( "~" , by)),
+      design = design,
+      FUN = svyquantile,
+      quantile = c(0, 0.25, 0.5, 0.75, 1),
+      vartype = "ci",
+      level = 0.95
     )
   
+  # Remove row names
   rownames(df) <- NULL
   
+  # Update columns names
   names(df) <- c(
     by, 
     "q0", "q25", "q50", "q75", "q100", 
@@ -19,9 +24,11 @@ get_adj_quantiles_by <- function(x, by, design) {
     "q0_ci_u", "q25_ci_u", "q50_ci_u", "q75_ci_u", "q100_ci_u"
   )
   
+  # Add a column containing the considered metric
   df$metric <- x
   
-  df <- df |> select(metric, everything())
+  # Reorder columns
+  df <- df |> dplyr::select(metric, everything())
   
   return(df)
 }
