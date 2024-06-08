dc76a6bfc6f93a536f07b6d5a6ed92d292dddaa2
Pierre-Yves de Müllenheim <pierre-yves.demullenheim@hotmail.fr>
2024-06-08 23:51:15 +0200

    reduced the number of quantiles computed

--- R/get_adj_quantiles_by.R
+++ R/get_adj_quantiles_by.R
@@ -8,7 +8,7 @@ get_adj_quantiles_by <- function(x, by, design) {
       by = as.formula(paste0( "~" , by)),
       design = design,
       FUN = svyquantile,
-      quantile = c(0, 0.25, 0.5, 0.75, 1),
+      quantile = c(0.25, 0.5, 0.75),
       vartype = "ci",
       level = 0.95
     )
@@ -19,9 +19,9 @@ get_adj_quantiles_by <- function(x, by, design) {
   # Update columns names
   names(df) <- c(
     by, 
-    "q0", "q25", "q50", "q75", "q100", 
-    "q0_ci_l", "q25_ci_l", "q50_ci_l", "q75_ci_l", "q100_ci_l",
-    "q0_ci_u", "q25_ci_u", "q50_ci_u", "q75_ci_u", "q100_ci_u"
+    "q25", "q50", "q75", 
+    "q25_ci_l", "q50_ci_l", "q75_ci_l",
+    "q25_ci_u", "q50_ci_u", "q75_ci_u"
   )
   
   # Add a column containing the considered metric
